3fae55525620b56b190000f67362c059dadfe2a0
Dozziny <ehgudwkd0813@gmail.com>
2023-04-14 01:20:37 +0900

    blog test

--- src/Post/calculation/HTML/index.js
+++ src/Post/calculation/HTML/index.js
@@ -1,4 +1,7 @@
 window.onload = function() {
+    const link = document.querySelector('a[href$="calc.html"]');
+    link.setAttribute('href', 'https://dozziny.github.io/src/post/calculation/HTML/calc.html');
+    link.setAttribute('target', '_blank');
     const calcButtons = document.querySelectorAll(".calc-button");
     for (let i = 0; i < calcButtons.length; i++) {
         calcButtons[i].setAttribute("onclick", "calcButton(this.innerHTML)");
